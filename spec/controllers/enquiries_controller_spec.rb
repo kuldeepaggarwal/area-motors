019c44ab446b2de05b754b9a9757d4a308f85a74
Kuldeep Aggarwal <kd.engineer@yahoo.co.in>
2018-08-14 10:56:52 +0530

    Add test case for synchronize controller request

---
+++ spec/controllers/enquiries_controller_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe EnquiriesController, type: :controller do
+  describe 'POST #synchronize' do
+    it 'synchronizes the enquires from third part service with our DB' do
+      expect { post :synchronize }.to change(Enquiry, :count).by(3)
+    end
+
+    it 'redirects to Enquiries listing page' do
+      post :synchronize
+      expect(response).to redirect_to(enquiries_path)
+    end
+  end
+end
