a02993238dd4bf7d27f809a1401a756587909a9f
Kuldeep Aggarwal <kd.engineer@yahoo.co.in>
2018-08-14 10:59:44 +0530

    Add more test cases

--- spec/controllers/enquiries_controller_spec.rb
+++ spec/controllers/enquiries_controller_spec.rb
@@ -6,6 +6,11 @@ describe EnquiriesController, type: :controller do
       expect { post :synchronize }.to change(Enquiry, :count).by(3)
     end
 
+    it 'does not create duplicate records' do
+      EnquirySynchronizer.synchronize
+      expect { post :synchronize }.not_to change(Enquiry, :count)
+    end
+
     it 'redirects to Enquiries listing page' do
       post :synchronize
       expect(response).to redirect_to(enquiries_path)
